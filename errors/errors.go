e359e558e5b67db947acd0dccecda8895aecadb6
Kerol <a.carollo@young.business>
2019-06-21 15:58:37 +0200

    added errors package

---
+++ errors/errors.go
@@ -0,0 +1,13 @@
+package errors
+
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+// DuplicateKey returns true if error rapresent Mongo error_code DuplicateKey
+func DuplicateKey(err error) bool {
+	if commandError, ok := err.(mongo.CommandError); ok {
+		return commandError == 11000
+	}
+	return false
+}
