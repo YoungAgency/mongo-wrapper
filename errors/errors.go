84f8fe1aad1312159d56742989e1f3b72434be64
Kerol <a.carollo@young.business>
2019-06-25 16:32:43 +0200

    fixed compile error

--- errors/errors.go
+++ errors/errors.go
@@ -7,7 +7,7 @@ import (
 // DuplicateKey returns true if error rapresent Mongo error_code DuplicateKey
 func DuplicateKey(err error) bool {
 	if commandError, ok := err.(mongo.CommandError); ok {
-		return commandError == 11000
+		return commandError.Code == 11000
 	}
 	return false
 }
