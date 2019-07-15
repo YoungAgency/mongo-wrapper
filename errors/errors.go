d0febdd71b3f716bd18ffc5a37127cdb5ebc0872
ciarmoli-marco <marco.ciarmoli@edu.unito.it>
2019-07-15 17:46:47 +0200

    Added WriteException handling for duplicate key

--- errors/errors.go
+++ errors/errors.go
@@ -13,5 +13,10 @@ func DuplicateKey(err error) bool {
 	if commandError, ok := err.(mongo.CommandError); ok {
 		return commandError.Code == duplicateCode
 	}
+	if writeEx, ok := err.(mongo.WriteException); ok {
+		if len(writeEx.WriteErrors) > 0 {
+			return writeEx.WriteErrors[0].Code == duplicateCode
+		}
+	}
 	return false
 }
