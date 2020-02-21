93c48b6b397b6907652b15b6a6404f5cda835a06
AndreKerol <a.carollo@young.business>
2020-02-21 11:31:36 +0100

    created PipelineBuilder struct

---
+++ query/pipeline_builder.go
@@ -0,0 +1,58 @@
+package query
+
+import (
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type PipelineBuilder struct {
+	pipeline []bson.D
+}
+
+func NewPipelineBuilder() *PipelineBuilder {
+	return &PipelineBuilder{
+		pipeline: make([]bson.D, 0, 2),
+	}
+}
+
+func (pb *PipelineBuilder) Match(value interface{}) *PipelineBuilder {
+	stage := bson.D{
+		bson.E{
+			Key:   "$match",
+			Value: value,
+		},
+	}
+	pb.pipeline = append(pb.pipeline, stage)
+	return pb
+}
+
+func (pb *PipelineBuilder) Sort(value interface{}) *PipelineBuilder {
+	stage := bson.D{
+		bson.E{
+			Key:   "$sort",
+			Value: value,
+		},
+	}
+	pb.pipeline = append(pb.pipeline, stage)
+	return pb
+}
+
+func (pb *PipelineBuilder) Project(value interface{}) *PipelineBuilder {
+	stage := bson.D{
+		bson.E{
+			Key:   "$project",
+			Value: value,
+		},
+	}
+	pb.pipeline = append(pb.pipeline, stage)
+	return pb
+}
+
+func (pb *PipelineBuilder) AppendStage(stage bson.D) *PipelineBuilder {
+	pb.pipeline = append(pb.pipeline, stage)
+	return pb
+}
+
+func (pb *PipelineBuilder) Pipeline() mongo.Pipeline {
+	return pb.pipeline
+}
