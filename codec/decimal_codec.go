c7e3eb28cf009d74df3112b3bc829209ff7740b4
AndreKerol <a.carollo@young.business>
2023-11-07 13:16:25 +0100

    feat: added write to distinct types on codec

--- codec/decimal_codec.go
+++ codec/decimal_codec.go
@@ -13,7 +13,11 @@ import (
 
 // DecimalCodec is used to encode decimal.Decimal types in primitive.Decimal128 and
 // decode primitive.Decimal128 in decimal.Decimal
+// if WriteInt is true, then it will write int32 or int64 for integer values
+// if AllowWriteInt32 is true, then it will write int32 for integer values if possible
 type DecimalCodec struct {
+	WriteInt        bool
+	AllowWriteInt32 bool
 }
 
 // EncodeValue implements bsoncodec.ValueEncoder interface
@@ -25,12 +29,22 @@ func (dc DecimalCodec) EncodeValue(ctx bsoncodec.EncodeContext, vw bsonrw.ValueW
 			Received: val,
 		}
 	}
-	ourDecimal := val.Interface().(decimal.Decimal)
-	mongoDecimal, err := decimalToPrimitive128(ourDecimal)
-	if err != nil {
-		return fmt.Errorf("DecimalCodec: unable to convert decimal.Decimal to primitive.Decimal128, %v", err)
+
+	dec := val.Interface().(decimal.Decimal)
+	if dc.WriteInt && dec.IsInteger() {
+		i64 := dec.IntPart()
+		if dc.AllowWriteInt32 && i64 > -2147483648 && i64 < 2147483647 {
+			return vw.WriteInt32(int32(i64))
+		} else {
+			return vw.WriteInt64(i64)
+		}
+	} else {
+		mongoDecimal, err := decimalToPrimitive128(dec)
+		if err != nil {
+			return fmt.Errorf("DecimalCodec: unable to convert decimal.Decimal to primitive.Decimal128, %v", err)
+		}
+		return vw.WriteDecimal128(mongoDecimal)
 	}
-	return vw.WriteDecimal128(mongoDecimal)
 }
 
 // DecodeValue implements bsoncodec.ValueEncoder interface
@@ -42,18 +56,50 @@ func (dc DecimalCodec) DecodeValue(ctx bsoncodec.DecodeContext, vr bsonrw.ValueR
 			Received: val,
 		}
 	}
-	if vr.Type() != bsontype.Decimal128 {
+	switch vr.Type() {
+	case bsontype.Decimal128:
+		mongoDecimal, err := vr.ReadDecimal128()
+		if err != nil {
+			return err
+		}
+		newDec, err := primitive128ToDecimal(mongoDecimal)
+		if err != nil {
+			return fmt.Errorf("DecimalCodec: unable to convert primitive.Decimal128 to decimal.Decimal %v", err)
+		}
+		val.Set(reflect.ValueOf(newDec))
+	case bsontype.Int32:
+		i32, err := vr.ReadInt32()
+		if err != nil {
+			return err
+		}
+		val.Set(reflect.ValueOf(decimal.NewFromInt32(i32)))
+	case bsontype.Int64:
+		i64, err := vr.ReadInt64()
+		if err != nil {
+			return err
+		}
+		val.Set(reflect.ValueOf(decimal.NewFromInt(i64)))
+	case bsontype.Double:
+		f64, err := vr.ReadDouble()
+		if err != nil {
+			return err
+		}
+		val.Set(reflect.ValueOf(decimal.NewFromFloat(f64)))
+	case bsontype.Null:
+		val.Set(reflect.ValueOf(decimal.Zero))
+	case bsontype.String:
+		str, err := vr.ReadString()
+		if err != nil {
+			return err
+		}
+		newDec, err := decimal.NewFromString(str)
+		if err != nil {
+			return fmt.Errorf("DecimalCodec: unable to convert string to decimal.Decimal %v", err)
+		}
+		val.Set(reflect.ValueOf(newDec))
+	default:
 		return fmt.Errorf("cannot decode %v into a decimal.Decimal type", vr.Type())
 	}
-	mongoDecimal, err := vr.ReadDecimal128()
-	if err != nil {
-		return err
-	}
-	newDec, err := primitive128ToDecimal(mongoDecimal)
-	if err != nil {
-		return fmt.Errorf("DecimalCodec: unable to convert primitive.Decimal128 to decimal.Decimal %v", err)
-	}
-	val.Set(reflect.ValueOf(newDec))
 	return nil
 }
 
