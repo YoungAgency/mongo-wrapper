3de379b8195c88c3ab44628f59abf659fcd2d0f1
AndreKerol <a.carollo@young.business>
2025-07-04 14:22:27 +0200

    feat: MongoTransactionRunner

--- transaction/exec.go
+++ transaction/exec.go
@@ -26,6 +26,37 @@ func ExecSnapshot(ctx context.Context, client *mongo.Client, fn ExecFn) (any, er
 	return res, err
 }
 
+type MongoTransactionRunner struct {
+	client *mongo.Client
+}
+
+func NewMongoTransactionRunner(client *mongo.Client) *MongoTransactionRunner {
+	return &MongoTransactionRunner{
+		client: client,
+	}
+}
+
+func (m *MongoTransactionRunner) ExecSnapshot(ctx context.Context, fn func(ctx context.Context) (any, error)) (any, error) {
+	// check if ctx is already a session context
+	if session := mongo.SessionFromContext(ctx); session != nil {
+		// If it is, we can use the existing session
+		return fn(ctx)
+	}
+	opt := mongoSnapshotOptions()
+	session, err := m.client.StartSession(opt)
+	if err != nil {
+		return nil, err
+	}
+	defer session.EndSession(context.Background())
+
+	// Use session.WithTransaction to run the function in a transaction
+	res, err := session.WithTransaction(ctx, func(ctx context.Context) (any, error) {
+		return fn(ctx)
+	})
+
+	return res, err
+}
+
 // mongoSnapshotOptions returns session options with snapshot readconcern.
 // If attached to Session, this options will be applied to all transaction runned within that session.
 func mongoSnapshotOptions() *options.SessionOptionsBuilder {
